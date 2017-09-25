21811d71eb5522e2b772d2354ed324990dc5d74d
Suguru Kato <salmon.bioinformatics@gmail.com>
2017-09-25 19:48:08 +0900

    Fix tscat

--- src/bin/tscat.rs
+++ src/bin/tscat.rs
@@ -13,6 +13,17 @@ fn print_usage(program: &str) {
     println!("{}", description);
 }
 
+fn skip_lines_until<R: BufRead, F>(reader: &mut R, f: F) -> std::io::Result<String>
+    where F: Fn(&str) -> bool {
+    loop {
+        let mut buf = String::new();
+        reader.read_line(&mut buf)?;
+        if f(&buf) {
+            return Ok(buf);
+        }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = &args[0];
@@ -35,7 +46,10 @@ fn main() {
         let file = File::open(filename).unwrap();
         let mut reader = BufReader::new(file);
 
-        skip_lines(&mut reader, 9).unwrap();
+        skip_lines(&mut reader, 10).unwrap();
+        print!("{}",
+               skip_lines_until(&mut reader,
+                                |s| s.len() >= 5 && s[..5].trim().is_empty()).unwrap());
 
         for line in reader.lines() {
             println!("{}", line.unwrap());
