13200df98c26192a8ddd479a5b6b23c890afa00d
Suguru Kato <salmon.bioinformatics@gmail.com>
2017-11-07 19:23:34 +0900

    Add contacts

---
+++ src/bin/contacts.rs
@@ -0,0 +1,58 @@
+extern crate cafetools;
+extern crate dcdio;
+
+use std::env;
+use std::process;
+use std::fs::File;
+use std::io::BufReader;
+use cafetools::native_info::NativeInfo;
+use dcdio::DcdReader;
+
+fn distance(x: &(f32, f32, f32), y: &(f32, f32, f32)) -> f32 {
+    ((x.0 - y.0).powi(2) + (x.1 - y.1).powi(2) + (x.2 - y.2).powi(2)).sqrt()
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        let program = &args[0];
+        eprintln!("Usage: {} NINFO STRUCTURE", program);
+        process::exit(1);
+    }
+
+    let ninfo = {
+        let file = File::open(&args[1]).unwrap();
+        let reader = BufReader::new(file);
+        NativeInfo::load(reader).unwrap()
+    };
+
+    let dcdreader = {
+        let file = File::open(&args[2]).unwrap();
+        DcdReader::new(file).unwrap()
+    };
+
+    let contacts = {
+        let mut contacts = ninfo.contacts;
+        contacts.sort_by(|x, y| x.index.cmp(&y.index));
+        contacts
+    };
+
+    print!("step");
+    for contact in &contacts {
+        print!(",{}", contact.index);
+    }
+    println!();
+
+    for frame in dcdreader.frames() {
+        let frame = frame.unwrap();
+        print!("{}", frame.step);
+        for contact in &contacts {
+            let &(ref index0, ref index1) = &contact.pair;
+            let dist = distance(&frame.positions[index0.index],
+                                &frame.positions[index1.index]) as f64;
+            print!(",{}", contact.length / dist);
+        }
+        println!();
+    }
+}
