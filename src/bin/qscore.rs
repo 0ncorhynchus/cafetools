3d2f6505cc7989f2fc2a9f71d98d0ec42740aaf8
Suguru Kato <salmon.bioinformatics@gmail.com>
2017-12-06 21:59:57 +0900

    Fix qscore

--- src/bin/qscore.rs
+++ src/bin/qscore.rs
@@ -48,7 +48,7 @@ fn main() {
             let &(ref index0, ref index1) = &contact.pair;
             let dist = distance(&frame.positions[index0.index-1],
                                 &frame.positions[index1.index-1]) as f64;
-            if dist < contact.length {
+            if dist < contact.length * 1.2 {
                 num_contacts += 1;
             }
         }
